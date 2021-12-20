5d89eee37b8e1ae02ce13b6b284a3faf3f26fa58
Artem Khabibullin <free2770@gmail.com>
2021-12-20 22:40:45 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const gameLogic = (rules, roundGenerator) => {
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer !== String(correctAnswer)) {
       console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
-      console.log(`Let's try again, ${userName}`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
     console.log('Correct!');
