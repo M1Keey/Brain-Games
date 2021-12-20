769b10bd5c904dd1d4d3f957e592ad8c55693fc9
Artem Khabibullin <free2770@gmail.com>
2021-12-20 21:04:00 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import readlineSync from 'readline-sync';
 
 const gameLogic = (rules, roundGenerator) => {
   const rounds = 3;
+
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
@@ -9,15 +10,17 @@ const gameLogic = (rules, roundGenerator) => {
   console.log(rules);
   for (let i = 0; i < rounds; i += 1) {
     const [question, correctAnswer] = roundGenerator();
+
     console.log(`Question: ${question}`);
+
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === String(correctAnswer)) {
-      console.log('Correct!');
-    } else if (userAnswer !== String(correctAnswer)) {
+    if (userAnswer !== String(correctAnswer)) {
       console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
       console.log(`Let's try again, ${userName}`);
       return;
     }
-  } console.log(`Congratulations, ${userName}!`);
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
 export default gameLogic;
