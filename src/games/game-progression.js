784d8d8e7bc40617fd3e80125bd97f6a8e503108
Artem Khabibullin <free2770@gmail.com>
2021-12-20 20:08:49 +0300

    fixes

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -2,25 +2,23 @@ import makeRandom from '../random.js';
 
 const rules = 'What number is missing in the progression?';
 
+const makeProgression = (firstItem, step) => {
+  const progression = [];
+  progression.push(firstItem);
+  for (let i = 0; i < 9; i += 1) {
+    progression.push(progression[i] + step);
+  } return progression;
+};
+
 const getRoundGenerator = () => {
   const firstItem = makeRandom(20, 1);
   const step = makeRandom(6, 1) + 1;
 
-  let question = [];
-  const progression = [];
-
-  const makeProgression = () => {
-    progression.push(firstItem);
-    for (let i = 0; i < 9; i += 1) {
-      progression.push(progression[i] + step);
-    } return progression;
-  };
-  makeProgression();
   const index = makeRandom(10, 0);
+  const progression = makeProgression(firstItem, step);
   const correctAnswer = progression[index];
-  const result = progression;
-  result[index] = '..';
-  question = result;
+  progression[index] = '..';
+  const question = progression;
   return [question, correctAnswer];
 };
 export { rules, getRoundGenerator };
